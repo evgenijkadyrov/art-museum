c84f59f54076513c51f0049bfc422c03ba200800
Yauheni Kadyrau <7077634@gmail.com>
2024-04-12 18:03:49 +0300

    conf: change config for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/book-landing-page/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
