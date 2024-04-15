7138a6d35a3c2d895e905b7b83e66b54bf5c27d3
Yauheni Kadyrau <7077634@gmail.com>
2024-04-15 20:33:10 +0300

    feat: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { defineConfig } from "vitest/config";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  test: {
+    environment: "jsdom",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
