2e87ee2984387b00693c8854b13691bf46b562e5
Yauheni Kadyrau <7077634@gmail.com>
2024-04-16 14:47:54 +0300

    feat: add tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-// import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
 import path from "path";
 
 export default defineConfig({
